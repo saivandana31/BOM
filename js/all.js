6ff088acfad19a8bfad7eb5506ece7113b411388
saivandana31 <saivandana31@gmail.com>
2022-09-01 21:23:05 +0530

    BOM files added

---
+++ js/all.js
@@ -0,0 +1,31 @@
+// console.log("window inner height----->",window.innerHeight);
+// console.log("window inner width----->",window.innerWidth);
+// console.log("window new----->",window.open());
+// console.log("window move to----->",window.moveTo);
+// console.log("screen  width----->",screen.width);
+// console.log("screen  height----->",screen.height);
+// console.log("screen avail width----->",screen.availWidth);
+// console.log("screen avail height----->",screen.availHeight);
+// console.log("screen colorDepth----->",screen.colorDepth);
+// console.log("screen pixel depth----->",screen.pixelDepth);
+// console.log("window location href----->",window.location.href);
+// console.log("window location host name----->",window.location.hostname);
+// console.log("window location pathname----->",window.location.pathname);
+// console.log("window location protocol----->",window.location.protocol);
+// console.log("window location port----->",window.location.port);
+// console.log("window location assign----->",window.location.assign("https://www.w3schools.com/bootstrap5/index.php"));
+// console.log("window history back----->",window.history.back());
+                // window alert
+                function demo(){
+                    alert("hello sai");
+                };
+                //   --------------confirmbox----------
+                function demo(){
+                    var text;
+                    if(confirm("press here")){
+                        text = "ok";
+                    }else{
+                        text = "cancel";
+                    };
+                }
+                
